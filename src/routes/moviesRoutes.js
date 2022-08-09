fc0051b0e0eddaa4c545cd5ad80b85bc8370a587
tsunodajapa <gabrieltsunoda@gmail.com>
2022-08-09 18:53:04 -0300

    feat: template aula 2 sequelize

--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -3,8 +3,19 @@ const router = express.Router();
 const moviesController = require("../controllers/moviesController");
 
 router.get("/movies", moviesController.index);
+router.get("/movies/:id", moviesController.show);
+
+//Extra
 //router.get('/movies/new', moviesController.new);
 //router.get('/movies/recommended', moviesController.recomended);
-router.get("/movies/:id", moviesController.show);
+
+router.get("/movies/add", moviesController.addMovieScreen);
+router.post("/movies", moviesController.create);
+
+router.get("/movies/edit/:id", moviesController.updateMovieScreen);
+router.put("/movies/:id", moviesController.update);
+
+router.get("/movies/edit/:id", moviesController.deleteMovieScreen);
+router.delete("/movies/:id", moviesController.delete);
 
 module.exports = router;
